c95efe95958fff8f2a740222e72584404f98ee53
Dai Kawahara <skbsporta7@gmail.com>
2021-05-23 10:00:18 +0900

    create project

---
+++ SwiftUI-training-API/SwiftUI-training-API/SwiftUI_training_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_training_APIApp.swift
+//  SwiftUI-training-API
+//
+//  Created by kawaharadai on 2021/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_training_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
