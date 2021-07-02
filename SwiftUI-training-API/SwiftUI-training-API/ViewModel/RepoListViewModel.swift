541d7c708a57ae7038e117fd3843b5f3bb3e5044
Dai Kawahara <skbsporta7@gmail.com>
2021-07-02 23:41:17 +0900

    add ViewModel

--- SwiftUI-training-API/SwiftUI-training-API/ViewModel/RepoListViewModel.swift
+++ SwiftUI-training-API/SwiftUI-training-API/ViewModel/RepoListViewModel.swift
@@ -8,12 +8,39 @@
 import Foundation
 import Combine
 
-protocol RepoListViewModel: ObservableObject {
-    func onAppear()
-    func onRetryButtonTapped()
-}
+final class RepoListViewModel: ObservableObject {
 
-final class RepoListViewModelImpl: RepoListViewModel {
-    func onAppear() {}
-    func onRetryButtonTapped() {}
+    @Published private(set) var state: Stateful<[Repo]> = .idle
+    private let repoRepository: RepoRepository
+    private var cancellables = Set<AnyCancellable>()
+
+    init(repoRepository: RepoRepository = RepoRepositoryImpl(repoAPIClient: RepoAPIClientImpl())) {
+        self.repoRepository = repoRepository
+    }
+
+    func onAppear() {
+        loadRepos()
+    }
+    
+    func onRetryButtonTapped() {
+        loadRepos()
+    }
+
+    private func loadRepos() {
+        repoRepository.fetchRepos()
+            .handleEvents(receiveSubscription: { [weak self] _ in
+                self?.state = .loading
+            })
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] (completion) in
+                switch completion {
+                case .failure(let error):
+                    self?.state = .failed(error)
+                case .finished: break
+                }
+            } receiveValue: { [weak self] (repos) in
+                self?.state = .loaded(repos)
+            }
+            .store(in: &cancellables)
+    }
 }
--- SwiftUI-training-API/SwiftUI-training-API/Views/RepoListView.swift
+++ SwiftUI-training-API/SwiftUI-training-API/Views/RepoListView.swift
@@ -9,18 +9,17 @@ import SwiftUI
 import Combine
 
 struct RepoListView: View {
-    @StateObject private var reposLoader = ReposLoader()
-    private var cancellables = Set<AnyCancellable>()
+    @StateObject private var viewModel = RepoListViewModel()
 
     var body: some View {
         NavigationView {
             Group {
-                switch reposLoader.state {
+                switch viewModel.state {
                 case .idle, .loading:
                     ProgressView("loading...")
                 case .failed(_):
                     RepoErrorView(retryAction: {
-                        reposLoader.call()
+                        viewModel.onRetryButtonTapped()
                     })
                 case .loaded(let repos):
                     if repos.isEmpty {
@@ -37,7 +36,7 @@ struct RepoListView: View {
             }
             .navigationTitle("Repositories")
         }.onAppear(perform: {
-            reposLoader.call()
+            viewModel.onAppear()
         })
     }
 }
